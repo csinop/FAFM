5ad554b422d54af52b56feaf7aae6255b1a9353a
Can Sinop <csinop.dev@gmail.com>
2022-07-05 20:04:23 +0300

    fixed a bug in moves tab
    fixed a bug regarding the reset functionality in moves tab

---
+++ java/com/example/fitnessappformyself/java/com/example/fitnessappformyself/main_menu_fragments/profile/StaticProfilePreferenceStrings.java
@@ -0,0 +1,18 @@
+package com.example.fitnessappformyself.main_menu_fragments.profile;
+
+public class StaticProfilePreferenceStrings {
+    public static final String SHARED_PREFERENCE_PROFILE_KEY = "Profile";
+    public static final String PROFILE_NAME = SHARED_PREFERENCE_PROFILE_KEY+".Name";
+    public static final String PROFILE_AGE = SHARED_PREFERENCE_PROFILE_KEY+".Age";
+    public static final String PROFILE_WEIGHT = SHARED_PREFERENCE_PROFILE_KEY+".Weight";
+    public static final String PROFILE_HEIGHT = SHARED_PREFERENCE_PROFILE_KEY+".Height";
+    public static final String PROFILE_GENDER = SHARED_PREFERENCE_PROFILE_KEY+".Gender";
+    public static final String PROFILE_BMR = SHARED_PREFERENCE_PROFILE_KEY+".BMR";
+    public static final String PROFILE_ACTIVITY = SHARED_PREFERENCE_PROFILE_KEY+".Activity";
+    public static final String PROFILE_CALORIES = SHARED_PREFERENCE_PROFILE_KEY+".Calories";
+    public static final String PROFILE_AVATAR = SHARED_PREFERENCE_PROFILE_KEY+".Avatar";
+    public static final String PROFILE_BODYFAT = SHARED_PREFERENCE_PROFILE_KEY+".BodyFat";
+    public static final String PROFILE_WAIST = SHARED_PREFERENCE_PROFILE_KEY+".Waist";
+    public static final String PROFILE_NECK = SHARED_PREFERENCE_PROFILE_KEY+".Neck";
+    public static final String PROFILE_HIP = SHARED_PREFERENCE_PROFILE_KEY+".Hip";
+}
